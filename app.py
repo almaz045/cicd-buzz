efc84a64355f24cfd42c74ebe0502210fc51659a
almaz045 <internet69s@mail.ru>
2020-04-20 11:49:16 -0400

    Heroku in Travis

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def generate_buzz():
     return page
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
+     # app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
+      app.run(host='0.0.0.0', port=int(os.getenv('PORT',5000))) # port 5000 is the default
+    # app.run(host='0.0.0.0', port=5000) # port 5000 is the default
