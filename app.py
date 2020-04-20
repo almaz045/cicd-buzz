b75d09456414566242b5356d7193ad1d3fe2aebe
almaz045 <internet69s@mail.ru>
2020-04-20 12:21:13 -0400

    Picture

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import signal
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
@@ -9,11 +9,12 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz()
-    page += '</h1></body></html>'
-    return page
-
+    #page = '<html><body><h1>'
+    #page += generator.generate_buzz()
+    #page += '</h1></body></html>'
+    #return page
+     buzz = generator.generate_buzz()
+     return render_template('index.html', buzz=buzz)
 if __name__ == "__main__":
      # app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
       app.run(host='0.0.0.0', port=int(os.getenv('PORT',5000))) # port 5000 is the default
