afd340947b33982fdc623550bf3c9818b8b96267
almaz045 <internet69s@mail.ru>
2020-06-01 14:37:29 -0400

    KAI

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "internet69s@mail.ru"
+  git config --global user.name "almaz045"
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout -b master
   git add . *.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin https://${GH_TOKEN}@github.com/almaz045/cicd-buzz.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master 
 }
 
 setup_git
